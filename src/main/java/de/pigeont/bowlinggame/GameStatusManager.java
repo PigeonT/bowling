383753758b957bd96d735dc27398383ed1bf0a99
pigeonT <pigeon.tian@gmail.com>
2017-03-09 21:33:17 +0100

    bowling ball

--- src/main/java/de/pigeont/bowlinggame/GameStatusManager.java
+++ src/main/java/de/pigeont/bowlinggame/GameStatusManager.java
@@ -7,11 +7,14 @@ import de.pigeont.bowlinggame.model.Pins;
 import de.pigeont.bowlinggame.model.Power;
 
 import java.util.List;
+import java.util.function.UnaryOperator;
 import java.util.logging.Logger;
 
-public final class GameStatusManager {
+final class GameStatusManager {
     private static GameStatusManager gameManager;
     private final Logger logger;
+    private final BallController ballController;
+    private final RoundController roundController;
     private GameController gameController;
     private Long gameControllerID = 0L;
     private Pins pins;
@@ -21,49 +24,120 @@ public final class GameStatusManager {
 
     private GameStatusManager(Logger l) {
         logger = l;
+        ballController = new BallController();
+        roundController = new RoundController();
     }
 
     @NotNull
-    public static GameStatusManager createGameStatusManager(@NotNull Logger lc) {
+    static GameStatusManager createGameStatusManager(@NotNull Logger lc) {
         gameManager = null == gameManager ? new GameStatusManager(lc) : gameManager;
         return gameManager;
     }
 
-    public void init() {
+    void init() {
         pins = new Pins();
-        ball = new Ball(15, 18);
+        ball = new Ball();
         power = new Power();
         hook = new Hook();
+        roundController.setGameStatusManager(this);
     }
 
-    public void update() {
-        if (!powerIsSet() || !hookIsSet()) return;
-
+    void update() {
+        if (isWaitingInput()) return;
+        //update ball position
+        ballController.update();
+        //update pins
+        pins.update(ball);
     }
 
-    public void setGameController(@NotNull GameController gameController) {
+    void setGameController(@NotNull GameController gameController) {
         if (0L != gameControllerID) throw new RuntimeException("reassignment of render of game constrolelr");
         this.gameController = gameController;
         gameControllerID = 1L;
     }
 
-    public List<Integer[]> getOriginPins() {
-        return Pins.ORIGIN_PIN;
-    }
-
-    public boolean powerIsSet() {
+    boolean powerIsSet() {
         return power.powerIsSet();
     }
 
-    public boolean hookIsSet() {
+    boolean hookIsSet() {
         return hook.hookIsSet();
     }
 
-    public void setPower(Integer power) {
+    boolean isWaitingInput() {
+        return roundController.isWaitingInput();
+    }
+
+    public void setWaitingInput(boolean newRound) {
+        roundController.setWaitingInput(newRound);
+    }
+
+    public void resetNewRound() {
+        ball.resetNewRound();
+        roundController.startNewRound();
+        hook.resetNewRound();
+        power.resetNewRound();
+    }
+
+    public Integer[] getBallPosition() {
+        return ball.getPosition();
+    }
+
+    public List<Integer[]> getPinsPosition() {
+        return pins.getPinsPosition();
+    }
+
+    Integer getPower() {
+        return power.getPower();
+    }
+
+    void setPower(Integer power) {
         this.power.setPower(power);
     }
 
-    public void setHook(Integer hook) {
+    Integer getHook() {
+        return hook.getHook();
+    }
+
+    void setHook(Integer hook) {
         this.hook.setHook(hook);
     }
+
+    private class BallController {
+        private BallController() {
+        }
+
+        void update() {
+            Hook h = GameStatusManager.this.hook;
+            Ball b = GameStatusManager.this.ball;
+            if (cached(b)) {
+                if (ball.checkResetCondition()) {
+                    gameController.resetNewRound();
+                    return;
+                }
+                ball.move();
+                return;
+            }
+            Integer v = h.getHook();
+
+            //offset is cot function
+            //math fomular is like:
+            //assuming h is hook value
+            //the x mean unit x length
+            //offset = f(h) = 16/(3/11 * |h - 23|) * x
+            double in = Double.valueOf(h.getHook());
+            UnaryOperator<Double> cal = a -> {
+                if (23D == a) return 0D;
+                return (5 * Math.abs(a - 23) / 23) / 16;
+            };
+            Double tmp = 23D < in ? cal.apply(in)
+                    : -cal.apply(in);
+            b.setOffsetVector(tmp);
+        }
+
+        boolean cached(Ball b) {
+            return b.isOffSetVectorSet();
+        }
+    }
+
 }
